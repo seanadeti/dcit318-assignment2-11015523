1c7f5d817f46b1e11b8fa31c4655e1b1a4f34b8b
Bless Adeti <blessadeti7@gmail.com>
2025-08-08 23:48:39 +0000

    Implemented Part 2: Abstract Classes and Methods updated

--- Program.cs
+++ Program.cs
@@ -5,7 +5,7 @@ class Program
 {
     static void Main(string[] args)
     {
-    
+        //Part 1
         Animal genericAnimal = new Animal();
         Dog myDog = new Dog();
         Cat myCat = new Cat();
@@ -14,6 +14,16 @@ class Program
         genericAnimal.MakeSound();
         myDog.MakeSound();
         myCat.MakeSound();
+        Console.WriteLine();
+
+
+        // Part 2
+        Circle myCircle = new Circle(5);
+        Rectangle myRectangle = new Rectangle(4, 6);
+
+        Console.WriteLine("Part 2: Abstract Classes and Methods");
+        Console.WriteLine($"Circle area: {myCircle.GetArea()}");
+        Console.WriteLine($"Rectangle area: {myRectangle.GetArea()}");
         Console.WriteLine();
     }
 }
